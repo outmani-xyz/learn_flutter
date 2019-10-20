5dcafd5886a7d00415d8d30a740afdb8efa5bee3
abdelhamid outmani <tilmido93@gmail.com>
2019-10-20 15:32:10 +0100

    try to show image in code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-void  main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar:AppBar(
-      title: Text('yeeeh'),
-    ),
-    body: Center(child: Text('hello '),),
-  ),
-));
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('yeeeh'),
+        ),
+        body: Center(
+          child: Image.asset(
+            'images/iq.png',
+            height: 100,
+            width: 100,
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+    ));
