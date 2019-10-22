1353d26aa21cc1f088789aaa5839d94afd4d6274
abdelhamid outmani <tilmido93@gmail.com>
2019-10-22 22:30:10 +0100

    add icon in column

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(HomePage());
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var center = CrossAxisAlignment.center;
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('yeeeh'),
+          backgroundColor: Colors.black87,
         ),
-        body: Center(
-          child: Image.asset(
-            'images/iq.png',
-            height: 100,
-            width: 100,
-          ),
-        ),
-        backgroundColor: Colors.blueGrey,
+        body: SafeArea(child:Column(
+          children: <Widget>[
+          Container(child: Icon(Icons.ac_unit,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
+          Container(child: Icon(Icons.access_time,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
+          Container(child: Icon(Icons.question_answer,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
+          Container(child: Icon(Icons.ac_unit,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
+          Container(child: Icon(Icons.ac_unit,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
+        ],
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        ) ,),
+backgroundColor: Colors.white24,
       ),
-    ));
+    
+    );
+  }
+}
