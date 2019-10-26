505b985ed655d2f6efe01e8f5220529e9e695ab4
abdelhamid outmani <tilmido93@gmail.com>
2019-10-26 13:08:43 +0100

    business card final

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,77 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(HomePage());
 
-
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var center = CrossAxisAlignment.center;
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('yeeeh'),
-          backgroundColor: Colors.black87,
+          title: Text('Business card'),
+          backgroundColor: Colors.black54,
         ),
-        body: SafeArea(child:Column(
-          children: <Widget>[
-          Container(child: Icon(Icons.ac_unit,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
-          Container(child: Icon(Icons.access_time,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
-          Container(child: Icon(Icons.question_answer,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
-          Container(child: Icon(Icons.ac_unit,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
-          Container(child: Icon(Icons.ac_unit,color: Colors.white54,size: 80,),color: Colors.blueGrey,margin: EdgeInsets.all(4),),
-        ],
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        ) ,),
-backgroundColor: Colors.white24,
+        body: SafeArea(
+            child: Container(
+          color: Colors.black87,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(
+                  'images/business_card.jpg',
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Hamid',
+                  style: TextStyle(fontSize: 24, color: Colors.white70),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(5),
+                color: Colors.white54,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone_android,
+                      size: 25,
+                    ),
+                    Text(
+                      '0669 000 000',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                      ),
+                    )
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+              Container(
+                padding: EdgeInsets.all(5),
+                color: Colors.white54,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.web,
+                      size: 25,
+                    ),
+                    Text(
+                      'http://utmani.xyz',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                      ),
+                    )
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+            ],
+          ),
+        )),
       ),
-    
     );
   }
 }
