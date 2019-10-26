c53041b86f19c8c20bef8621ccca79647c294e4f
abdelhamid outmani <tilmido93@gmail.com>
2019-10-26 13:30:00 +0100

    finish business card

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,12 @@ class HomePage extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.access_time),
+              onPressed: ()=>null,
+            )
+          ],
           title: Text('Business card'),
           backgroundColor: Colors.black54,
         ),
@@ -29,43 +35,47 @@ class HomePage extends StatelessWidget {
                   style: TextStyle(fontSize: 24, color: Colors.white70),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(5),
+              Card(
                 color: Colors.white54,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone_android,
-                      size: 25,
-                    ),
-                    Text(
-                      '0669 000 000',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone_android,
+                        size: 25,
                       ),
-                    )
-                  ],
+                      Text(
+                        '0669 000 000',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
                 margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               ),
-              Container(
-                padding: EdgeInsets.all(5),
+              Card(
                 color: Colors.white54,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.web,
-                      size: 25,
-                    ),
-                    Text(
-                      'http://utmani.xyz',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.web,
+                        size: 25,
                       ),
-                    )
-                  ],
+                      Text(
+                        'http://outmani.xyz',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
                 margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               ),
