4cc569d2ad762dc5171e2b1800580e66bef8962a
abdelhamid outmani <tilmido93@gmail.com>
2019-11-10 16:40:08 +0100

    add clickable btn with func

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,40 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int diceLeft = 1;
+  int diceRight = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+     
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+            child: Image.asset('images/dice$diceLeft.png'),
+            onPressed: getDice,
+          )),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$diceRight.png'),
+              onPressed: getDice,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  getDice(){
+    setState(() {
+     diceLeft = Random().nextInt(6) +1;
+     diceRight = Random().nextInt(6) +1;
+    });
   }
 }
